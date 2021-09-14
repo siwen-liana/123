9b86c08cacbf5f022aaa58183cf9fd0bc72891ea
siwen-liana <81551575+siwen-liana@users.noreply.github.com>
2021-09-14 15:29:30 +0800

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,12 @@
+def init(params: dict):
+    return None
+
+
+def run(data: dict, func=None) -> bool:
+    if "1" in "objectPath" or "2" in "objectPath":
+        return True
+    return False
+
+
+def teardown():
+    pass
